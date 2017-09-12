c4c2f3d3b77df4ccdd4b95f3657f053505be99e3
aubreyghoward <aubrey.g.howard@rice.edu>
2017-09-12 16:14:34 -0500

    Question1A&BComplete

--- ImageRead.m
+++ ImageRead.m
@@ -1,8 +1,7 @@
-% 1. (a) Write a function that reads in an image and displays it so that 1%
-% of the pixels are black and 1% of the pixels are white. Hint: your
-% function can call the stretchlim function, see the help for that function.
-% (b) Write a second function where the fraction of pixels to display as
-% black or white are equal and can be specified as a second input to this
-% function. (c) Redo part (b) but make the option to specify the fraction
-% of black/white pixels optional with a default value of 1% if not
-% specified. 
+
+function [adjimg] = ImageRead(inputs)
+%will fix an image for you so that the bottom and top 1% of the pixals are 
+%adjusted white/black.
+    img = imread(inputs);
+    adjimg = imshow(img,stretchlim(img))
+    end
--- inclass5.m
+++ inclass5.m
@@ -3,9 +3,16 @@
 % 1. (a) Write a function that reads in an image and displays it so that 1%
 % of the pixels are black and 1% of the pixels are white. Hint: your
 % function can call the stretchlim function, see the help for that function.
+img = ImageRead('X-ray-crystallography-DNA-i.gif');
+
+%%
 % (b) Write a second function where the fraction of pixels to display as
 % black or white are equal and can be specified as a second input to this
-% function. (c) Redo part (b) but make the option to specify the fraction
+% function. 
+
+img = ImageAdj('X-ray-crystallography-DNA-i.gif',0.1,0.9);
+
+%(c) Redo part (b) but make the option to specify the fraction
 % of black/white pixels optional with a default value of 1% if not
 % specified. 
 
