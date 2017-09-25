da7442dbf8fc1eae9b4e90f2202ffddde5e7ef20
warmflasha <warmflasha@gmail.com>
2017-09-25 12:27:52 -0500

    graded inclass5

--- inclass5.m
+++ inclass5.m
@@ -1,3 +1,5 @@
+%AW: See comments below 0.6/1. Come talk to me if they don't make sense. 
+
 %Inclass assignment 5. 
 
 % 1. (a) Write a function that reads in an image and displays it so that 1%
@@ -12,11 +14,17 @@ img = ImageRead('Rosalind_Franklin.jpg');
 clear all
 img = ImageAdj('Rosalind_Franklin.jpg',0.2,0.8);
 
+%AW: Your functions do not work. Try running it with your high value low (say 0.01),
+%then the entire image should be white. This doesn't happen with your images. It always looks the same. -0.3.
+% Also note. your returned value isn't an image, it is a handle to the image. 
+
 %(c) Redo part (b) but make the option to specify the fraction
 % of black/white pixels optional with a default value of 1% if not
 % specified. 
 
 %%
+%AW: For this code, try note to repeat things inside the if/else statement. 
+% Put common code outside the conditional and only the part that differs inside.
 img = ImageAdj2('Rosalind_Franklin.jpg',0.01,0.95)
 %%
 clear all
@@ -31,6 +39,12 @@ img = ImageAdj2 ()
 %is, returns the complementary base pair for each base, read from end
 %to beginning. (e.g. the reverse compliment of ATGC is GCAT). Do not use the
 %builtin MATLAB function for this. 
+
+%AW: Your code here complements but does not reverse. -0.1.
+% no need for the statement bases=seq; just use seq.
+% also no need for a cell array here, you can just use a string. 
+
 clear all
+
 
 cdna = hwcdna('GcTaatatattat')
